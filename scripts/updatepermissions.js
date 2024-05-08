345c9b2f54bb647a5dd176a36d14f50f406ed3e1
amahmoud85 <amahmoud85@outlook.com>
2024-05-09 00:44:12 +0400

    Update updatepermissions.js

--- scripts/updatepermissions.js
+++ scripts/updatepermissions.js
@@ -13,7 +13,7 @@ fs.readFile( manifestFile, "utf8", function( err, data )
 
     var result = data;
     for (var i=0; i<permissionsToRemove.length; i++)
-        result = result.replace( "READ_EXTERNAL_STORAGE", "WRITE_EXTERNAL_STORAGE" );
+        result = result.replace("<uses-permission android:name=\"android.permission." + permissionsToRemove[i] + "\" />", "" );
 
     fs.writeFile( manifestFile, result, "utf8", function( err )
     {
